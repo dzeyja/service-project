1f94c8976c5abdb609887ade6bdea887536377a9
Daulet <Daulet>
2024-11-03 01:11:34 +0500

    add absolute imports fro fsd

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,8 @@ import { BuildPaths } from './config/build/types/config'
 const paths: BuildPaths = {
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html')
+  html: path.resolve(__dirname, 'public', 'index.html'),
+  src: path.resolve(__dirname, 'src')
 }
 
 const mode = 'development'
