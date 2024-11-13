46691340ae81b06ef6c225ea302fb8dcb9f37a95
Daulet <Daulet>
2024-11-14 02:01:34 +0500

    add order form

--- src/pages/HomePage/ui/OrderForm/OrderForm.tsx
+++ src/pages/HomePage/ui/OrderForm/OrderForm.tsx
@@ -1,27 +1,65 @@
 import { classNames } from "shared/lib/classNames/classNames"
 import cls from './OrderForm.module.scss'
-import { Input } from "shared/ui/Input/Input"
-import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button"
+import { useRef } from "react"
+import emailjs from '@emailjs/browser';
+import { Input } from "shared/ui/Input/Input";
+import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button";
+
 
 interface OrderFormProps {
     className?: string
 }
 
 export function OrderForm({ className }: OrderFormProps) {
+    const form = useRef();
 
-    return (
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm('service_mmo6uzq', 'template_4n125dg', form.current, {
+            publicKey: 'j8--Ih-F0S9ZhQFfs',
+        })
+        .then(
+            () => {
+            console.log('SUCCESS!');
+            },
+            (error) => {
+            console.log('FAILED...', error.text);
+            },
+      );
+   };
+
+   return (
         <div className={classNames(cls.OrderForm, {}, [className])}>
-            <form className={cls.form}>
-                <Input className={cls.input}/>
-                <Input className={cls.input}/>
+            <form ref={form} className={cls.form}>
+                <div className={cls.title}>
+                    Оставьте заявку
+                </div>
+                <Input 
+                    placeholder="Имя" 
+                    type='text' 
+                    name="user_name"
+                />
+                <Input 
+                    placeholder="Email" 
+                    type='email' 
+                    value="zharylkasynov_d@mail.ru"
+                    name="user_email"
+                />
+                <Input 
+                    placeholder="Сообщение" 
+                    name="message"
+                />
                 <Button 
-                    square
-                    theme={ButtonTheme.PRIMARY}
-                    size={ButtonSize.M}    
+                    square 
+                    theme={ButtonTheme.PRIMARY} 
+                    size={ButtonSize.M} 
+                    onClick={sendEmail}
                 >
-                    Заказать
+                    Отправить заявку
                 </Button>
             </form>
-        </div>
-    )
+         </div>
+   )
 }
